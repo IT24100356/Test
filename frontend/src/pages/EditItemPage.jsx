438b62cb3239aa8f605118113178bdb17ff409aa
SadeshRaj <sadeshrajapakshe@gmail.com>
2026-04-29 09:57:37 +0530

    Initial commit

---
+++ frontend/src/pages/EditItemPage.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import ItemForm from "../components/ItemForm.jsx";
+import { getItemById, updateItem } from "../api/itemApi.js";
+
+function EditItemPage() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [item, setItem] = useState(null);
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      try {
+        const { data } = await getItemById(id);
+        setItem(data);
+        setItem({ //updated from here
+      ...data,
+      date: data.date ? data.date.split("T")[0] : "",
+    }); //to here
+      } catch (error) {
+        console.error("Failed to fetch item", error);
+      }
+    };
+
+    fetchItem();
+  }, [id]);
+
+  const handleUpdate = async (formData) => {
+    try {
+      await updateItem(id, formData);
+      navigate("/");
+    } catch (error) {
+      console.error("Failed to update item", error);
+      alert("Failed to update item");
+    }
+  };
+
+  if (!item) return <p>Loading item details...</p>;
+
+  return (
+    <ItemForm
+      initialValues={item}
+      submitText="Update Item"
+      onSubmit={handleUpdate}
+    />
+  );
+}
+
+export default EditItemPage;
